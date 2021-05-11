3f68dfaa434b64a65b48c390b6e31cab90d7317f
kotkotei <kotkotei@yandex.ru>
2021-05-11 17:21:51 +0700

    veiotnetohnieh

---
+++ Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {///1.46
+            Console.Write("Ввведи сюда ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ввведи сюда ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ввведи сюда ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int s = b * c;
+            int v = s * a;
+            Console.Write("Площадь: " + s + " Объем : " + v);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//5.7
+            const double ITEM_PRICE = 20.4;
+            for (int i = 2; i <= 20; i++)
+            {
+                Console.WriteLine("Цена за {0} едениц товара: {1} рублей", i, ITEM_PRICE * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
